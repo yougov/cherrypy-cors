5fa3a7423b6365091533972668884f482c0dc86f
Jason R. Coombs <jaraco@jaraco.com>
2016-09-06 12:13:11 -0400

    Update example to use spec-consistent origin.

--- simple-example.py
+++ simple-example.py
@@ -6,7 +6,7 @@ requirements and run it directly with Python.
 Once the server is running, connect to it to verify
 that the CORS functionality is present::
 
-    $ curl -v -H "Origin: www.mysite.org" http://localhost:8080
+    $ curl -v -H "Origin: http://www.mysite.org" http://localhost:8080
     * Rebuilt URL to: http://localhost:8080/
     *   Trying 127.0.0.1...
     * Connected to localhost (127.0.0.1) port 8080 (#0)
@@ -14,14 +14,14 @@ that the CORS functionality is present::
     > Host: localhost:8080
     > User-Agent: curl/7.43.0
     > Accept: */*
-    > Origin: www.mysite.org
+    > Origin: http://www.mysite.org
     >
     < HTTP/1.1 200 OK
     < Date: Mon, 01 Aug 2016 15:40:34 GMT
     < Vary: Origin
     < Content-Type: text/html;charset=utf-8
     < Server: CherryPy/7.1.0
-    < Access-Control-Allow-Origin: www.mysite.org
+    < Access-Control-Allow-Origin: http://www.mysite.org
     < Content-Length: 21
     <
     {"value": "success"}
